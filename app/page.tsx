628ba0b776cfff9b9776c78e72ea232a2fa52cb0
DonNorris <your-github-dnorris@frii.com>
2026-09-20 16:42:39 -0700

    Make email list image full width

--- app/page.tsx
+++ app/page.tsx
@@ -98,7 +98,7 @@ setJoined(params.get("joined") === "1");
 </section>
 <img src="/artist-success-center.png" alt="Artist Success Center" style={{ width: "100%", display: "block" }} />
 <section id="email-signup" style={section}>
- <img src="/join-email-list.png" alt="Join Our Email List" style={{ width: "100vw", marginLeft: "calc(50% - 50vw)", display: "block" }} />
+ <img src="/join-email-list.png" alt="Join Our Email List" style={{ width: "200%", maxWidth: "none", display: "block" }} />
 <form action="/api/email-signup" method="POST" style={{ margin: "10px auto", maxWidth: 600 }}>
   <input
     type="email"
