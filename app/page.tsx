4d216615f7189707262b364fe678d237c624b7e0
DonNorris <your-github-dnorris@frii.com>
2026-09-22 10:32:28 -0700

    Remove email signup width constraint

--- app/page.tsx
+++ app/page.tsx
@@ -98,7 +98,7 @@ setJoined(params.get("joined") === "1");
 </section>
 <img src="/artist-success-center.png" alt="Artist Success Center" style={{ width: "100%", display: "block" }} />
 <div style={{ position: "relative", width: "100vw", marginLeft: "calc(50% - 50vw)", padding: 0 }}>
-<section id="email-signup" style={section}>
+<section id="email-signup">
  <img src="/join-email-list.png" alt="Join Our Email List" style={{ width: "100%", display: "block" }} />
 <form action="/api/email-signup" method="POST" style={{ position: "absolute", top: "32%", left: "50%", transform: "translateX(-50%)", width: "46%", maxWidth: 600, zIndex: 2 }}>
   <input
